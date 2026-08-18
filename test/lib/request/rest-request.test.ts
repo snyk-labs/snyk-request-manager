8c8964dbec052859316344de49ba8713f0f0dce5
gwnlng <gwunleong.lee@snyk.io>
2026-08-18 14:12:35 +0800

    fix: node version dep
    revised build-test to use Node24
    updated axios, nock libs

--- test/lib/request/rest-request.test.ts
+++ test/lib/request/rest-request.test.ts
@@ -317,5 +317,5 @@ describe('Test Snyk Utils error handling/classification', () => {
       expect(err).toBeInstanceOf(GenericError);
       expect(err.message.config.headers.Authorization).toBe('****');
     }
-  });
+  }, 35_000); // mock delays 32s to exceed makeSnykRequest's 30s axios timeout
 });
